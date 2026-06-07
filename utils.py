23b09607f79fd2bfd1afd5c5a7f34fd70c580242
redoydollar <redoydollar@gmail.com>
2026-06-07 19:48:59 +0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,3 @@
 import re
 
-ADULT_KEYWORDS = [
-    "xxx", "porn", "adult", "18+", "sex", "erotic", "nude", "hentai",
-    "jav", "brazzers", "bangbros", "xvideo", "xhamster", "hardcore",
-    "softcore
+ADULT_KEYWORDS = ["xxx", "porn", "adult", "18+", "sex", "erotic", "nude", "hentai", "jav", "brazzers", "bangbros", "xvideo", "xhamster", "hardcore", "softcore", "onlyfans", "le

de8f507033e02cb13d31f065932347e5885efd35
redoydollar <redoydollar@gmail.com>
2026-06-08 22:06:32 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# এখানে BotFather থেকে পাওয়া টোকেন দিন
+API_ID = 12345678  # my.telegram.org থেকে পাওয়া সংখ্যা
+API_HASH = "your_api_hash_here"  # my.telegram.org থেকে পাওয়া হ্যাশ
+BOT_TOKEN = "your_bot_token_here"  # BotFather থেকে পাওয়া টোকেন
+
+# MongoDB Atlas থেকে পাওয়া URI (পাসওয়ার্ড দিয়ে দিন)
+MONGO_URI = "mongodb+srv://<username>:<password>@cluster0.xxxxx.mongodb.net/?retryWrites=true&w=majority"
+DB_NAME = "RenameBotDB"
